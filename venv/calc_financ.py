f2e7c061264b42e3443b6fd820d9507536542c0a
20151inf0182 <eduardomeurer999@gmail.com>
2018-10-05 12:09:58 -0300

    Esta entrando nos ifs

--- venv/calc_financ.py
+++ venv/calc_financ.py
@@ -1,8 +1,72 @@
 from tkinter import *
+from calculo import *
 
 
+def calcular(opt, entry):
 
-janela= Tk();
+    if (opt == 0):
+        valorInicial = None
+        valorFinal = float(edtValorFinal.get())
+        taxaJuros = float(edtPercentJuros.get())
+        tempoApli = float(edtTempo.get())
+        if (varTipo.get() == 1):
+            tempoApli = (tempoApli / 360) / 100
+        else:
+            tempoApli = (tempoApli / 12) / 100
+        cCapital = calculo.calculo(valorInicial, tempoApli, valorFinal, taxaJuros)
+        entry["text"] = cCapital.calculaValInicial(var.get())
+        entry.place(x=200, y=50)
+
+    elif (opt == 1):
+        valorInicial = float(edtValorInicial.get())
+        valorFinal = float(edtValorFinal.get())
+        taxaJuros = float(edtPercentJuros.get())
+        tempoApli = None
+        cTempo = calculo(valorInicial, tempoApli, valorFinal, taxaJuros)
+
+        if (varTipo.get() == 1):
+            entry["text"] = cTempo.calculaTempo(var.get(), varTipo.get())
+            entry.place(x=200, y=80)
+        else:
+            entry["text"] = cTempo.calculaTempo(var.get(), varTipo.get())
+            entry.place(x=200, y=80)
+
+    elif (opt == 2):
+        valorInicial = float(edtValorInicial.get())
+        valorFinal = None
+        taxaJuros = float(edtPercentJuros.get())
+        tempoApli = float(edtTempo.get())
+        if (varTipo.get() == 1):
+            tempoApli = (tempoApli / 360) / 100
+        else:
+            tempoApli = (tempoApli / 12) / 100
+        cMontante = calculo.calculo(valorInicial, tempoApli, valorFinal, taxaJuros)
+        entry["text"] = cMontante.calculaMontante(var.get())
+        entry.place(x=200, y=110)
+
+    elif (opt == 3):
+        valorInicial = float(edtValorInicial.get())
+        valorFinal = float(edtValorFinal.get())
+        taxaJuros = None
+        tempoApli = float(edtTempo.get())
+
+        if (varTipo.get() == 1):
+            tempoApli = tempoApli/360
+        else:
+            tempoApli = tempoApli / 12
+
+        cTaxa = calculo(valorInicial, tempoApli, valorFinal, taxaJuros)
+        entry["text"] = cTaxa.calculapJuros(var)
+        entry.place(x=200, y=140)
+        # s["text"] = "valor: "+str(cTaxa.calculapJuros(var))
+        # s.place(x=500, y= 200)
+
+    else:
+        s["text"] = "A opção selecionada é inválida"
+        s.place(x=200, y=170)
+
+
+janela = Tk();
 L = "1200"
 A = "800"
 E = "100"
@@ -13,59 +77,76 @@ janela.title("Calculo Financeiro")
 janela["bg"] = "green"
 corJanela = janela["bg"]
 
+var = IntVar()
+varTipo = IntVar()
+R1 = Radiobutton(janela, bg=corJanela, text="Juros Composto", variable=var, value=1)
+R2 = Radiobutton(janela, bg=corJanela, text="Juros Simples", variable=var, value=2)
+
+R1.place(x=100, y=170)
+R2.place(x=100, y=200)
+
+Label(janela, bg=corJanela, text="Tipo de taxa: ").place(x=300, y=170)
+R1 = Radiobutton(janela, bg=corJanela, text="a.a", variable=varTipo, value=1)
+R2 = Radiobutton(janela, bg=corJanela, text="a.m", variable=varTipo, value=2)
+
+R1.place(x=300, y=200)
+R2.place(x=300, y=230)
+
 Label(janela,
       text="Calculos financeiros",
       bg=corJanela,
-      font="Arial 12").place(500, 10)
+      font="Arial 12").place(x=500, y=10)
 
 Label(janela,
       bg=corJanela,
-      text="Valor inicial: ").place(100, 50)
+      text="Valor inicial: ").place(x=80, y=50)
 
 edtValorInicial = Entry(janela,
-                        width=20); edtValorInicial.place(150, 50)
+                        width=20);
+edtValorInicial.place(x=200, y=50)
 
 Button(janela,
        bg="blue",
        text="Calcular",
-       command=lambda: calcular(0)).place(200, 50)
+       command=lambda: calcular(0, edtValorInicial)).place(x=350, y=50)
 
 Label(janela,
       bg=corJanela,
-      text="Tempo de aplicação: ").place(100, 80)
+      text="Tempo de aplicação: ").place(x=80, y=80)
 
-edtTempo = Entry(janela, 
-                 width=20); edtTempo.place(150, 80)
+edtTempo = Entry(janela,
+                 width=20);
+edtTempo.place(x=200, y=80)
 Button(janela,
        bg="blue",
        text="Calcular",
-       command=lambda: calcular(1)).place(200, 80)
+       command=lambda: calcular(1, edtTempo)).place(x=350, y=80)
 
 Label(janela,
       bg=corJanela,
-      text="Valor final: ").place(100, 110)
+      text="Valor final: ").place(x=80, y=110)
 
 edtValorFinal = Entry(janela,
-                      width = 20); edtValorFinal.place(150, 110)
+                      width=20);
+edtValorFinal.place(x=200, y=110)
 
 Button(janela,
        bg="blue",
        text="Calcular",
-       command=lambda: calcular(2)).place(200, 110)
+       command=lambda: calcular(2, edtValorFinal)).place(x=350, y=110)
 
 Label(janela,
       bg=corJanela,
-      text="% de juros: ").place(100, 140)
+      text="% de juros: ").place(x=80, y=140)
 
 edtPercentJuros = Entry(janela,
-                        bg=corJanela); edtPercentJuros.place(150,140)
+                        width=20);
+edtPercentJuros.place(x=200, y=140)
 Button(janela,
        bg="blue",
        text="Calcular",
-       command=lambda: calcular(3)).place(200, 140)
+       command=lambda: calcular(3, edtPercentJuros)).place(x=350, y=140)
 
+s = Label(janela, bg=corJanela)
 
-
-
-
-
+janela.mainloop()
--- venv/calculo.py
+++ venv/calculo.py
@@ -2,69 +2,68 @@ from math import *
 
 class calculo():
     def __init__(self, vInicial, tempo, vFinal, pJuros):
-        self.tempo = tempo;
-        self.vFinal = vFinal;
-        self.pJuros = pJuros;
-        self.vInicial = vInicial;
-
-    def setTempo(self, tempo):
         self.tempo = tempo
-    def getTempo(self):
-        return self.tempo
-
-    def setvFinal(self, vFinal):
         self.vFinal = vFinal
-    def getFinal(self):
-        return self.vFinal
-
-
-    def setvInicial(self, vInicial):
-        self.vInicial = vInicial
-    def getvInicial(self):
-        return self.vInicial
-
-    def setpJuros(self, pJuros):
         self.pJuros = pJuros
-    def getpJuros(self):
-        return self.pJuros
+        self.vInicial = vInicial
 
-    def calculaTempo(self, isComposto):
-        if(isComposto):
+    def calculaTempo(self, isComposto, isAA):
+        if(isComposto == 1):
+            
             if(self.tempo == None and self.pJuros != None and self.vInicial != None and self.vFinal != None):
-                self.tempo = log1p(self.vFinal/self.vInicial)/log1p(1+ (self.pJuros/100))
-                arrReturn = {"a.a": self.tempo/360,
-                             "a.m": self.tempo/12}
+                if(isAA):
+                    self.tempo = log10(self.vFinal/self.vInicial)/log10(1+ (self.pJuros/100))
+                    arrReturn = self.tempo/360
+                else:
+                    self.tempo = log10(self.vFinal / self.vInicial) / log10(1 + (self.pJuros / 100))
+                    arrReturn = self.tempo / 12
+
             else:
                 return "Somente o campo 'Tempo de aplicação' pode ficar em branco'"
 
         else:
             if (self.tempo == None and self.pJuros != None and self.vInicial != None and self.vFinal != None):
                 self.tempo = self.vFinal-self.vInicial/(self.vInicial*self.pJuros)
-                arrReturn = {"a.a": self.tempo/360,
-                             "a.m": self.tempo/12}
+                arrReturn = self.tempo
 
         return arrReturn
 
     def calculaMontante(self, isComposto):
-        if (isComposto):
-            if (self.tempo != None and self.pJuros == None and self.vInicial != None and self.vFinal == None):
-                self.tempo = log1p(self.vFinal / self.vInicial) / log1p(1 + (self.pJuros / 100))
-                arrReturn = {"a.a": self.tempo / 360,
-                             "a.m": self.tempo / 12}
+        if (self.tempo != None and self.pJuros != None and self.vInicial != None and self.vFinal == None):
+            if (isComposto == 1):
+                self.vFinal = self.vInicial*(1+self.pJuros)**self.tempo
+                arrReturn = self.vFinal
             else:
-                return "Somente o campo 'Tempo de aplicação' pode ficar em branco'"
-
+                self.vFinal = self.vInicial + (1+(self.pJuros*self.tempo))
+                arrReturn = self.vFinal
         else:
-            if (self.tempo == None and self.pJuros != None and self.vInicial != None and self.vFinal == None):
-                self.tempo = self.vFinal - self.vInicial / (self.vInicial * self.pJuros)
-                arrReturn = {"a.a": self.tempo / 360,
-                             "a.m": self.tempo / 12}
-
-        return arrReturn
+            return "Somente o campo 'Montante' pode ficar em branco'"
 
     def calculaValInicial(self, isComposto):
-        if(isComposto):
-            self.vInicial = self.v
+        if (self.tempo != None and self.pJuros != None and self.vInicial == None and self.vFinal != None):
+            if(isComposto == 1):
+                self.vInicial = self.vFinal/(1+self.pJuros)**self.tempo
+                arrReturn = self.vInicial
+            else:
+                self.vInicial = self.vFinal - (1+(self.pJuros*self.tempo))
+                arrReturn = self.vInicial
+            return arrReturn
+        else:
+            return "Somente o campo 'Valor inicial' pode ficar em branco'"
+
+    def calculapJuros(self, isComposto):
+        if (self.tempo != None and self.pJuros == None and self.vInicial != None and self.vFinal != None):
+            if(isComposto == 1):
+                self.pJuros = self.tempo//(self.vFinal/self.vInicial)-1
+
+                return self.pJuros
+            else:
+                self.pJuros = (self.vFinal-self.vInicial-1)/self.tempo
+                return self.pJuros
+        else:
+            return "Somente o campo 'Taxa de juros' pode ficar em branco"
+
+
 
 
 
